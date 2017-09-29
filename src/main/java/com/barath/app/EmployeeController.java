aaabf0adaf7b858d7078b29d85a6269f981c05c5
Barath <barathmacdec91@gmail.com>
2017-09-29 23:01:45 +0530

    Revert "Revert "INITIAL COMMIT""
    This reverts commit 32dd3cf8ff8f00f81c0d627fcee49358ffbe0212.

---
+++ src/main/java/com/barath/app/EmployeeController.java
@@ -0,0 +1,43 @@
+package com.barath.app;
+
+import javafx.scene.media.Media;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/employee")
+public class EmployeeController {
+
+    private  EmployeeService employeeService;
+
+    public EmployeeController(EmployeeService employeeService){
+        this.employeeService=employeeService;
+    }
+    @PostMapping(value = "/create",produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public Employee createEmployee(@RequestBody Employee employee){
+
+        return employeeService.saveEmployee(employee);
+    }
+
+    @PostMapping(value = "/create",produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public List<Employee> createEmployees(@RequestBody List<Employee> employees){
+        return employeeService.saveEmployees(employees);
+    }
+
+    @GetMapping(value = "/getAll",produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public List<Employee> findAll(){
+        return employeeService.findAll();
+    }
+
+    @GetMapping(value = "/get",produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public Employee findEmployee(@RequestParam String employeeId){
+        return employeeService.findEmployee(employeeId);
+    }
+
+    @GetMapping(value = "/getByName",produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public List<Employee> findEmployeeByName(@RequestParam  String employeeName){
+        return employeeService.findEmployeeByName(employeeName);
+    }
+}
